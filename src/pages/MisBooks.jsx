7f6221ea8eb48c02a401f84d633efd98f8c35ba8
Innavoj <145022054+Innavoj@users.noreply.github.com>
2024-08-22 16:47:50 -0400

    Agregando Componnentes My Books

--- src/pages/MisBooks.jsx
+++ src/pages/MisBooks.jsx
@@ -1,10 +1,27 @@
-import { Box } from "@mui/material";
+
+import { Box, Typography } from "@mui/material";
+import CardBooks from "../components/CardBooks";
+
+const libros = [
+  { id: 1, title: "The Best Select",  img: "/src/assets/img/1.jpg", precio: 20.00 },
+  { id: 2, title: "Vida y Obra de JM",  img: "/src/assets/img/2.jpg", precio: 45.00 },
+  { id: 3, title: "Operacion Condor",  img: "/src/assets/img/3.jpg", precio: 50.00 },
+  { id: 4, title: "Quimica",  img: "/src/assets/img/4.jpg", precio: 90.00 },
+
+]
 
 export default function MisBooksPages() {
+  const libro = libros.map(i => 
+    <li key={i.id} >{i.title} - {i.precio}</li>
+  )
+
   return (
     <>
-      <Box sx={{ flexGrow: 1,  bgcolor: "background.default", p: 3, textAlign: 'center' }}>
-        <h1>My Books</h1>
+      <Box sx={{ flexGrow: 2,  bgcolor: "background.default", p: '4rem', m: '4rem', textAlign: 'center' }}>
+        <Typography variant="h6"> My Books</Typography>
+        <CardBooks />
+    
+        
       </Box>
     </>
   );
